790cf5386505c4cbcccd914c3e3e1e0e3b0f279d
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-05-25 14:36:14 +0200

    Added class Bounds. New make

---
+++ src/com/smokebox/lib/utils/geom/Bounds.java
@@ -0,0 +1,34 @@
+package com.smokebox.lib.utils.geom;
+
+/**
+ * Created by Harald on 25.5.15.
+ */
+public class Bounds {
+
+  int x;
+  int y;
+  int width;
+  int height;
+
+  public Bounds(int x, int y, int width, int height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  public boolean overlaps(Bounds b) {
+    return (x < b.x + b.width) && (x + width > b.x) && (y < b.y + b.height) && (
+        y + height > b.y);
+  }
+
+  public boolean overlapsList(Iterable<Bounds> boundsList) {
+    for (Bounds bounds : boundsList) {
+      if (overlaps(bounds)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
